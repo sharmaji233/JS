0409fb36c1a8318a4a0b520821b69629fc1023a7
Sharma G <talk2sharmaji7@gmail.com>
2025-06-02 21:47:35 +0530

    3

---
+++ 01_Basic/03_conversationOperation.js
@@ -0,0 +1,27 @@
+let score = "33abc"
+
+// console.log(typeof score);
+
+// console.log(typeof score);
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+
+// console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => Nan
+// true => 1
+// false => 0
+
+let isLoggedIn = "panku"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn);
+
+
+// 1 => true
+// 0 => false
+// "" => false 
+// "panku" => true
